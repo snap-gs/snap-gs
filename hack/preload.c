76ad99a7ec083535b544179575c488d4fff6de14
C Anthony Risinger <c@anthonyrisinger.com>
2022-06-30 15:26:53 -0500

    wip

--- hack/preload.c
+++ hack/preload.c
@@ -20,11 +20,13 @@ char *snapgs_lobby_listen2 = NULL;
 // Largest is "\x07\x2b000.000.000.000:00000|000.000.000.000:00000\x00".
 size_t send_buf1_size = 0;
 size_t send_buf2_size = 0;
-char send_buf1[46] = {0};
-char send_buf2[46] = {0};
+unsigned char send_buf1[46] = {0};
+unsigned char send_buf2[46] = {0};
 
 
 ssize_t (*send_next)(int, const void *, size_t, int);
+ssize_t (*recv_next)(int, void *, size_t, int);
+ssize_t (*recvfrom_next)(int, void *restrict, size_t, int, struct sockaddr *restrict, socklen_t *restrict);
 
 
 void _init(void) {
@@ -37,6 +39,12 @@ void _init(void) {
     if ((send_next = dlsym(RTLD_NEXT, "send")) == NULL) {
         fprintf(stderr, "preload.c: _init: dlerror: %s\n", dlerror());
         exit(90);
+    } else if ((recv_next = dlsym(RTLD_NEXT, "recv")) == NULL) {
+        fprintf(stderr, "preload.c: _init: dlerror: %s\n", dlerror());
+        exit(91);
+    } else if ((recvfrom_next = dlsym(RTLD_NEXT, "recvfrom")) == NULL) {
+        fprintf(stderr, "preload.c: _init: dlerror: %s\n", dlerror());
+        exit(92);
     }
 
     if ((snapgs_lobby_listen = getenv("SNAPGS_LOBBY_LISTEN")) == NULL) {
@@ -80,8 +88,101 @@ void _init(void) {
     send_buf1[0] = 7, send_buf1[1] = len+1+len1, send_buf1[2+len] = '|';
     send_buf2[0] = 7, send_buf2[1] = len+1+len1, send_buf2[2+len+len1-len2] = '|', send_buf2[2+len+len1-len2-2] = ':';
 
+    if (len == 19 && len1 == 19 && len2 == 16)
+        // memcpy(&send_buf2[2], "3.33.251.70:27002|fast-001.snap.gs:8978", 39);
+        // memcpy(&send_buf2[2], "0.0.0.0:0|18.224.29.212:27002|0.0.0.0:0", 39);
+        memcpy(&send_buf2[2], "0.0.0.0:0|0.0.0.0:0|18.224.29.212:27002", 39);
+
 }
 
+ssize_t recv(int fd, void *buf, size_t size, int flags) {
+    return recv_next(fd, buf, size, flags);
+}
+
+ssize_t recvfrom(int fd, void *restrict buf, size_t len, int flags,
+                 struct sockaddr *restrict addr, socklen_t *restrict addrlen) {
+
+    ssize_t size = recvfrom_next(fd, buf, len, flags, addr, addrlen);
+    if (size < sizeof(send_buf1) || size == -1)
+        return size;
+
+    unsigned char *data = (unsigned char *) buf;
+    unsigned char *last = (unsigned char *) buf + size;
+    while ((data = memchr(data, 7, last-data)) != NULL) {
+
+        if (last-data < 21)
+            break;
+
+        unsigned int ips_size = data[1];
+        if (ips_size < 19 || ips_size > 43) {
+            if (++data < last)
+                continue;
+
+            break;
+        }
+
+        unsigned char *p = NULL;
+        unsigned char *c = &data[2];
+
+        while (c < data + 2 + ips_size) {
+            switch (c[0]) {
+                case '|':
+                    p = c;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case ':':
+                case '.':
+                    c++;
+                    continue;
+                default:
+                    p = NULL;
+            }
+            break;
+        }
+
+        if (p == NULL) {
+            if (++data < last)
+                continue;
+
+            break;
+        }
+
+        unsigned char a1[22] = {0};
+        unsigned char a2[22] = {0};
+
+        memcpy(&a1, &data[2], p-data-2);
+        memcpy(&a2, p+1, data+1+ips_size-p);
+
+        if (p-data-2 == data+1+ips_size-p) {
+
+            fprintf(stderr, "preload.c: recvfrom <<< \"%s|%s\" (%d)\n", (char*)&a1, (char*)&a2, ips_size);
+
+            memcpy(&data[2], "0000000000000000:0", p-data-2);
+            // memcpy(&data[2], &a2, p-data-2);
+            // memcpy(p+1, &a1, data+1+ips_size-p);
+
+            unsigned char a3[44] = {0};
+            memcpy(&a3, &data[2], ips_size);
+
+            fprintf(stderr, "preload.c: recvfrom >>> \"%s\" (%d)\n", (char*)&a3, ips_size);
+
+        }
+
+        if ((data += 2 + ips_size) >= last)
+            break;
+
+    }
+
+    return size;
+};
 
 ssize_t send(int fd, const void *buf, size_t size, int flags) {
 
